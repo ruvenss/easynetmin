22ded0231566bf1e1699378844832b7947e215af
Ruvenss G Wilches <ruvenss@gmail.com>
2014-04-04 12:56:52 +0200

    Create config_template.php

---
+++ config_template.php
@@ -0,0 +1,11 @@
+<?php
+// Description : Configuration template file
+class phpCFG {
+  var $location = "http://127.0.0.1/";
+  var $language = "en";
+  var $dbhost = "localhost";
+  var $dbuser = "root";
+  var $dbpass = "";
+  var $dbase = "easynetmin";
+}
+?>
